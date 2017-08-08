7dbe56b466b5132d78dfa597ad70381d81b64f62
Aloïs Deniel <alois.deniel@gmail.com>
2017-08-08 22:28:47 +0200

    Added Forms samples.

---
+++ Sources/Microcharts.Samples.Forms/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace Microcharts.Samples.Forms
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+
+			var charts = Data.CreateQuickstart();
+			this.chart1.Chart = charts[0];
+			this.chart2.Chart = charts[1];
+			this.chart3.Chart = charts[2];
+			this.chart4.Chart = charts[3];
+			this.chart5.Chart = charts[4];
+		}
+	}
+}
