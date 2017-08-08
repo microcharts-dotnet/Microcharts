456070f2427d23baac878737d53d88c77992f229
Aloïs Deniel <alois.deniel@gmail.com>
2017-08-08 21:28:11 +0200

    Added PCL and Forms projects

---
+++ Sources/Microcharts.Portable.Forms/ChartView.cs
@@ -0,0 +1,37 @@
+﻿namespace Microcharts.iOS
+{
+	using Xamarin.Forms;
+	using SkiaSharp.Views.Forms;
+
+	public class ChartView : SKCanvasView
+	{
+		public ChartView()
+		{
+			this.BackgroundColor = Color.Transparent;
+			this.PaintSurface += OnPaintCanvas;
+		}
+
+		private Chart chart;
+
+		public Chart Chart
+		{
+			get => this.chart;
+			set
+			{
+				if (this.chart != value)
+				{
+					this.chart = value;
+					this.InvalidateSurface();
+				}
+			}
+		}
+
+		private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
+		{
+			if (this.chart != null)
+			{
+				this.chart.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+			}
+		}
+	}
+}
