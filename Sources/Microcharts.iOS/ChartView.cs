c8f71d88522b560e77afa1f75839a8faf741c13d
nieminen <nieminen432@gmail.com>
2018-11-15 20:46:21 -0600

    Minor formatting updates;

--- Sources/Microcharts.iOS/ChartView.cs
+++ Sources/Microcharts.iOS/ChartView.cs
@@ -1,45 +1,45 @@
 ﻿#if __IOS__
 namespace Microcharts.iOS
 {
-    using UIKit;
-    using SkiaSharp.Views.iOS;
+	using UIKit;
+	using SkiaSharp.Views.iOS;
 #else
 namespace Microcharts.macOS
 {
-    using SkiaSharp.Views.Mac;
+	using SkiaSharp.Views.Mac;
 #endif
 
-    public class ChartView : SKCanvasView
-    {
-        public ChartView()
-        {
-#if __IOS__
-                        this.BackgroundColor = UIColor.Clear;
-#endif
-                        this.PaintSurface += OnPaintCanvas;
-                }
+	public class ChartView : SKCanvasView
+	{
+		public ChartView()
+		{
+			#if __IOS__
+			this.BackgroundColor = UIColor.Clear;
+			#endif
+			this.PaintSurface += OnPaintCanvas;
+		}
 
-                private Chart chart;
+		private Chart chart;
 
-                public Chart Chart 
-                {
-                        get => this.chart;
-                        set
-                        {
-                                if(this.chart != value)
-                                {
-                                        this.chart = value;
-                                        this.SetNeedsDisplayInRect(this.Bounds);
-                                }
-                        }
-                }
+		public Chart Chart
+		{
+			get => this.chart;
+			set
+			{
+				if (this.chart != value)
+				{
+					this.chart = value;
+					this.SetNeedsDisplayInRect(this.Bounds);
+				}
+			}
+		}
 
-                private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
-                {
-                        if (this.chart != null)
-                        {
-                                this.chart.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
-                        }
-                }
-        }
+		private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
+		{
+			if (this.chart != null)
+			{
+				this.chart.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+			}
+		}
+	}
 }
