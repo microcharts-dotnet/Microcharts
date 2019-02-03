a3e5af9ca282efbdfef6dff520e26d4af5954d44
Ned Lanon <ned.lanon@outlook.com>
2019-02-03 19:28:00 +0100

    Added UWP support.

---
+++ Sources/Microcharts.Uwp/ChartView.cs
@@ -0,0 +1,32 @@
+﻿namespace Microcharts.Uwp
+{
+    using SkiaSharp.Views.UWP;
+
+    public class ChartView : SKXamlCanvas
+    {
+        public ChartView()
+        {
+            this.PaintSurface += OnPaintCanvas;
+        }
+
+        private Chart chart;
+
+        public Chart Chart
+        {
+            get => this.chart;
+            set
+            {
+                if (this.chart != value)
+                {
+                    this.chart = value;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void OnPaintCanvas(object sender, SKPaintSurfaceEventArgs e)
+        {
+            this.chart.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+        }
+    }
+}
