a909cc0594bdffb8e28b9d62193eb100ef964b05
Ned Lanon <ned.lanon@outlook.com>
2019-02-03 19:28:00 +0100

    Fixed code format issues.

--- Sources/Microcharts.Forms/ChartView.cs
+++ Sources/Microcharts.Forms/ChartView.cs
@@ -1,4 +1,7 @@
-﻿namespace Microcharts.Forms
+﻿// Copyright (c) Aloïs DENIEL. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microcharts.Forms
 {
 	using Xamarin.Forms;
 	using SkiaSharp.Views.Forms;
