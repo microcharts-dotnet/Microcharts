8afa6c417cde56cd2867b8828c941b93b9d75035
JMOrbegoso <60024333+JMOrbegoso@users.noreply.github.com>
2020-08-02 11:49:42 -0500

    Added ChartView views

--- Sources/Microcharts.Samples.Wpf/MainWindow.xaml.cs
+++ Sources/Microcharts.Samples.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,13 @@ namespace Microcharts.Samples.Wpf
         public MainWindow()
         {
             InitializeComponent();
+            var charts = Data.CreateQuickstart();
+            this.chart1.Chart = charts[0];
+            this.chart2.Chart = charts[1];
+            this.chart3.Chart = charts[2];
+            this.chart4.Chart = charts[3];
+            this.chart5.Chart = charts[4];
+            this.chart6.Chart = charts[5];
         }
     }
 }
