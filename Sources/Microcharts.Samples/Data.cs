aab58d900c1d3411f911d8afcc8a90e5ce16d345
Ed Lomonaco <elomonaco127@gmail.com>
2020-08-09 14:32:57 -0400

    Fixed letter spacing issue

--- Sources/Microcharts.Samples/Data.cs
+++ Sources/Microcharts.Samples/Data.cs
@@ -307,32 +307,68 @@ namespace Microcharts.Samples
             {
                 new ChartEntry(200)
                 {
-                        Label = "January",
+                        Label = "Week 1",
                         ValueLabel = "200",
                         Color = SKColor.Parse("#266489"),
                 },
                 new ChartEntry(400)
                 {
-                        Label = "February",
+                        Label = "Week 2",
                         ValueLabel = "400",
                         Color = SKColor.Parse("#68B9C0"),
                 },
                 new ChartEntry(100)
                 {
-                        Label = "March",
+                        Label = "Week 3",
                         ValueLabel = "100",
                         Color = SKColor.Parse("#90D585"),
                 },
+                new ChartEntry(600)
+                {
+                    Label = "Week 4",
+                    ValueLabel = "600",
+                    Color = SKColor.Parse("#32a852"),
+                }
             };
 
             return new Chart[]
             {
-                new BarChart() { Entries = entries, LabelTextSize = 60, LabelOrientation = Orientation.Horizontal },
-                new PointChart() { Entries = entries, LabelTextSize = 60, LabelOrientation = Orientation.Horizontal },
-                new LineChart() { Entries = entries, LabelTextSize = 60, LabelOrientation = Orientation.Horizontal },
-                new DonutChart() { Entries = entries, LabelTextSize = 60 },
-                new RadialGaugeChart() { Entries = entries, LabelTextSize = 60 },
-                new RadarChart() { Entries = entries, LabelTextSize = 60 },
+                new BarChart
+                {
+                    Entries = entries,
+                    LabelTextSize = 55,
+                    LabelOrientation = Orientation.Horizontal,
+                    Margin = 10
+                },
+                new PointChart
+                {
+                    Entries = entries,
+                    LabelTextSize = 55,
+                    LabelOrientation = Orientation.Horizontal,
+                    Margin = 10
+                },
+                new LineChart
+                {
+                    Entries = entries,
+                    LabelTextSize = 55,
+                    LabelOrientation = Orientation.Horizontal,
+                    Margin = 10
+                },
+                new DonutChart
+                {
+                    Entries = entries,
+                    LabelTextSize = 60
+                },
+                new RadialGaugeChart
+                {
+                    Entries = entries,
+                    LabelTextSize = 60
+                },
+                new RadarChart
+                {
+                    Entries = entries,
+                    LabelTextSize = 60
+                }
             };
         }
 
