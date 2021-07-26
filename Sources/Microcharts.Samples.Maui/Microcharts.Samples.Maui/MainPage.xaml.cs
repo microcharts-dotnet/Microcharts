123e0f2a02cdbd8e29cbe31b7becac23a69be7a9
Morgan SOULLEZ <morgan.soullez@gmail.com>
2021-07-26 20:51:45 +0200

    Start of something new !

---
+++ Sources/Microcharts.Samples.Maui/Microcharts.Samples.Maui/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace Microcharts.Samples.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+        }
+    }
+}
---
+++ Sources/Microcharts.Samples.Maui/Microcharts.Samples.Maui/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Hosting;
+
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace Microcharts.Samples.Maui
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+        }
+    }
+}
