0616cb892f698b4ef506db65dce3df15a4e40057
Tore Fuglestad <tore@fuglestad.no>
2022-01-21 15:31:08 +0100

    whitespace fix : honor project editorconfig settings

--- Sources/Microcharts.Eto/ChartView.cs
+++ Sources/Microcharts.Eto/ChartView.cs
@@ -3,27 +3,27 @@ namespace Microcharts.Eto
 {
     using global::Eto.SkiaDraw;
     public class ChartView : SkiaDrawable
-	{
-		Chart? _chart;
-		public ChartView()
-		{
-		}
+    {
+        Chart? _chart;
+        public ChartView()
+        {
+        }
 
-		public Chart? Chart
-		{
-			get => _chart;
-			set
-			{
-				_chart = value;
-				this.Invalidate();
-			}
-		}
+        public Chart? Chart
+        {
+            get => _chart;
+            set
+            {
+                _chart = value;
+                this.Invalidate();
+            }
+        }
         protected override void OnPaint(SKPaintEventArgs e)
         {
-			if (_chart != null)
-				_chart.Draw(e.Surface.Canvas, Width, Height);
-			else
+            if (_chart != null)
+                _chart.Draw(e.Surface.Canvas, Width, Height);
+            else
                 e.Surface.Canvas.Clear();
-		}
-	}
+        }
+    }
 }
--- Sources/Microcharts.Samples.Eto/Program.cs
+++ Sources/Microcharts.Samples.Eto/Program.cs
@@ -3,12 +3,12 @@ using Eto.Forms;
 
 namespace Microcharts.Samples.Eto
 {
-	static class Program
-	{
-		[STAThread]
-		static void Main(string[] args)
-		{
-			new Application(global::Eto.Platform.Detect).Run(new MainForm());
-		}
-	}
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new Application(global::Eto.Platform.Detect).Run(new MainForm());
+        }
+    }
 }
